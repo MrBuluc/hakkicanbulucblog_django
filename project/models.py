bba9f26a9695b2f826218267a746faf6f627a9e8
HAKKICAN <hakkicanbuluc@gmail.com>
2020-10-28 15:31:30 +0300

    added CkEditor

--- project/models.py
+++ project/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 
@@ -7,7 +8,7 @@ class Project(models.Model):
     author = models.ForeignKey(
         "auth.User", on_delete=models.CASCADE, verbose_name="Yazar")
     title = models.CharField(max_length=50, verbose_name="Başlık")
-    content = models.TextField(verbose_name="İçerik")
+    content = RichTextField()
     created_date = models.DateTimeField(
         auto_now_add=True, verbose_name="Oluşturulma Tarihi")
 
