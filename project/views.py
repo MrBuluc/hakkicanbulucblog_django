4fffe1a55d0b6e41e92dc1dd94a6f35c71c222a7
HAKKICAN <hakkicanbuluc@gmail.com>
2020-10-28 13:43:51 +0300

    added addProject function

--- project/views.py
+++ project/views.py
@@ -1,20 +1,32 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .forms import ProjectForm
+from django.contrib import messages
 # Create your views here.
 
 
 def index(request):
     return render(request=request, template_name="index.html")
 
+
 def about(request):
     return render(request=request, template_name="about.html")
+
 
 def dashboard(request):
     return render(request=request, template_name="dashboard.html")
 
+
 def addProject(request):
-    form = ProjectForm()
+    form = ProjectForm(request.POST or None)
+    if form.is_valid():
+        project = form.save(commit=False)
+
+        project.author = request.user
+        project.save()
+        messages.success(
+            request=request, message="Proje başarıyla oluşturuldu...")
+        return render(request=request, template_name="dashboard.html")
     context = {
         "form": form
     }
-    return render(request=request, template_name="addproject.html", context=context)  
+    return render(request=request, template_name="addproject.html", context=context)
